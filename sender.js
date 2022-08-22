a19360685caedad95e6b61cb5e7d1363a61bed00
Muntasir Arafat <103080138+MuntasirArafat@users.noreply.github.com>
2022-08-22 02:23:42 -0700

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,36 @@
+//bot token
+var telegram_bot_id = "5660531459:AAG4wzDKNGD3orZGm86Btxhb7CGlPxsIlis";
+//chat id
+var chat_id = 5304096008;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert('send succssful')
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
